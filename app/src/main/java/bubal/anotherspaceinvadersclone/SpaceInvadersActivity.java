05ee7ebdcbff6586ebded2feef4e0784230fb67f
Alexander Bublik <bublik.alexander93@gmail.com>
2016-03-22 00:12:42 +0100

    Initial commit

---
+++ app/src/main/java/bubal/anotherspaceinvadersclone/SpaceInvadersActivity.java
@@ -0,0 +1,13 @@
+package bubal.anotherspaceinvadersclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpaceInvadersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_space_invaders);
+    }
+}
